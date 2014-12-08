3ba094a30f2eb892b71a7607c208efbb78720bb6
ali <ali@Rapture.(none)>
2014-12-08 20:07:34 +0500

    Added general use func .GetN(n,order)

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package streamy
 
-type filter func(int) bool
+type Filter func(int) bool
+
+var (
+	// the order in which list of values will be evaluted
+	OrderAsc  = true
+	OrderDesc = false
+)
 
 type stream struct {
 	length  int
-	filters []filter
+	filters []Filter
 }
 
 func New(length int) *stream {
@@ -13,41 +19,69 @@ func New(length int) *stream {
 	}
 }
 
-func (s *stream) Filter(f filter) *stream {
+func (s *stream) Filter(f Filter) *stream {
 	s.filters = append(s.filters, f)
 	return s
 }
 
-func (s *stream) GetFirst() (int, bool) {
-	var (
-		j int
-	)
-	for i := 0; i < s.length; i++ {
+func (s *stream) GetN(n int, order bool) (res []int) {
+	if n < 0 {
+		// what u want!
+		return
+	}
+	fChain := func(i int) bool {
+		var j int
 		for j = 0; j < len(s.filters); j++ {
 			if !s.filters[j](i) {
 				break
 			}
 		}
 		if j == len(s.filters) {
-			return i, true
+			return true
+		}
+		return false
+	}
+
+	if order == OrderAsc {
+		for i := 0; i < s.length; i++ {
+			if fChain(i) {
+				res = append(res, i)
+				if len(res) == n {
+					return
+				}
+			}
+		}
+	} else {
+		for i := s.length - 1; i >= 0; i-- {
+			if fChain(i) {
+				res = append(res, i)
+				if len(res) == n {
+					return
+				}
+			}
 		}
 	}
-	return 0, false
+	return
 }
 
 func (s *stream) GetAll() (res []int) {
-	var (
-		j int
-	)
-	for i := 0; i < s.length; i++ {
-		for j = 0; j < len(s.filters); j++ {
-			if !s.filters[j](i) {
-				break
-			}
-		}
-		if j == len(s.filters) {
-			res = append(res, i)
-		}
+	return s.GetN(0, OrderAsc)
+}
+
+func (s *stream) GetFirst() (int, bool) {
+	res := s.GetN(1, OrderAsc)
+	if len(res) == 0 {
+		return -1, false
+	} else {
+		return res[0], true
 	}
-	return res
+}
+
+func (s *stream) GetLast() (int, bool) {
+	res := s.GetN(1, OrderDesc)
+	if len(res) == 0 {
+		return -1, false
+	} else {
+		return res[0], true
+	}
 }
--- main_test.go
+++ main_test.go
@@ -47,9 +47,68 @@ func TestGetAll(t *testing.T) {
 
 	if len(m) != 4 {
 		t.Fail()
-	} else {
-		for _, idx := range m {
-			log.Println(d[idx])
+	}
+}
+
+func TestMap(t *testing.T) {
+	d := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+	var mp []int
+	streamy.New(len(d)).Filter(
+		filterEven(&d),
+	).Filter(
+		func(i int) bool {
+			mp = append(mp, d[i]*10)
+			return true
+		},
+	).GetAll()
+}
+
+func filterEven(data *[]int) streamy.Filter {
+	return func(i int) bool {
+		if (*data)[i]%2 == 0 {
+			return true
 		}
+		return false
+	}
+}
+
+func TestGetLast(t *testing.T) {
+	d := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 21}
+	n, ok := streamy.New(len(d)).Filter(filterEven(&d)).GetLast()
+	if d[n] != 8 {
+		log.Fatal("wrong number")
+		t.Fail()
+	}
+	if !ok {
+		log.Fatal("Returned nothing")
+		t.Fail()
+	}
+}
+
+func TestGetN(t *testing.T) {
+	d := []int{1, 44, 8, 2, 3, 4, 5, 7}
+	evens := streamy.New(len(d)).Filter(filterEven(&d)).GetN(2, streamy.OrderDesc)
+	if len(evens) != 2 {
+		log.Println("Wrong number of evens returned", len(evens))
+		t.Fail()
+	}
+	if d[evens[0]] != 4 {
+		log.Println("Wrong order")
+		t.Fail()
+	}
+	if d[evens[1]] != 2 {
+		log.Println("Wrong order")
+		t.Fail()
+	}
+
+	d = []int{1, 4}
+	evens = streamy.New(len(d)).Filter(filterEven(&d)).GetN(0, streamy.OrderAsc)
+	if len(evens) != 1 {
+		log.Println("Wrong number of evens returned")
+		t.Fail()
+	}
+	if d[evens[0]] != 4 {
+		log.Println("Wrong number")
+		t.Fail()
 	}
 }
